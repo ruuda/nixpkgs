5b1572b9055e459881b376bc03589cc1805e733f
Ruud van Asseldonk <dev@ruuda.nl>
2026-05-01 22:07:13 +0200

    libressl_4_1: delete unsupported package
    LibreSSL branches are supported for one year after the OpenBSD release
    in which they are included. LibreSSL 4.1 was part of OpenBSD 7.7, which
    was released on April 28, 2025, so it's end of life now.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7003,7 +7003,6 @@ with pkgs;
   zunclient = with python313Packages; toPythonApplication python-zunclient;
 
   inherit (callPackages ../by-name/li/libressl { })
-    libressl_4_1
     libressl_4_2
     libressl_4_3
     ;
